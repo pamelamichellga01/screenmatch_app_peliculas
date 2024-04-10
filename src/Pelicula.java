b00a396d122e9846f9b81e242607fef5e9461fd1
pamelamichellga01 <72150148+pamelamichellga01@users.noreply.github.com>
2024-04-10 16:24:01 -0500

    Subiendo cambios del proyecto

--- src/Pelicula.java
+++ src/Pelicula.java
@@ -1,2 +1,28 @@
 public class Pelicula {
+
+    String nombre;
+    int fechaDeLanzamiento;
+    int duracionEnMinutos;
+
+    boolean incluidoEnElplan;
+
+    double sumaDeLasEvaluaciones;
+
+    int totalDeLasEvaluaciones;
+
+
+    void muestraFichaTecnica() {
+        System.out.println("Mi pelicula es: " + nombre);
+        System.out.println("Su fecha de lanzamiento es: " + fechaDeLanzamiento);
+        System.out.println("Duracion en minutos " + duracionEnMinutos);
+    }
+
+    void evalua(double nota){
+        sumaDeLasEvaluaciones += nota;
+        totalDeLasEvaluaciones ++;
+    }
+
+    double calculaMedia(){
+        return sumaDeLasEvaluaciones/totalDeLasEvaluaciones;
+    }
 }
