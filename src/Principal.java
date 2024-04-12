38f2282adb73f137742b48db81ef4dfb43dce98d
pamelamichellga01 <72150148+pamelamichellga01@users.noreply.github.com>
2024-04-12 10:09:46 -0500

    avances del proyecto

--- src/Principal.java
+++ src/Principal.java
@@ -1,30 +1,22 @@
+import com.aluracursos.screenmatch.modelos.Pelicula;
+
 public class Principal {
     public static void main(String[] args) {
 
         Pelicula miPelicula = new Pelicula();
 
-        miPelicula.nombre="Encanto";
-        miPelicula.fechaDeLanzamiento=2021;
-        miPelicula.duracionEnMinutos= 120;
+        miPelicula.setNombre("Encanto");
+        miPelicula.setFechaDeLanzamiento(2021);
+        miPelicula.setDuracionEnMinutos(120);
+        miPelicula.setIncluidoEnElplan(true);
 
         miPelicula.muestraFichaTecnica();
 
         miPelicula.evalua(7.8);
         miPelicula.evalua(10);
 
-        System.out.println(miPelicula.sumaDeLasEvaluaciones);
-        System.out.println(miPelicula.totalDeLasEvaluaciones);
+        System.out.println(miPelicula.getTotalDeLasEvaluaciones());
         System.out.println(miPelicula.calculaMedia());
-
-
-
-        Pelicula otraPelicula = new Pelicula();
-
-        otraPelicula.nombre= "Matrix";
-        otraPelicula.fechaDeLanzamiento= 1998;
-        otraPelicula.duracionEnMinutos= 180;
-
-        otraPelicula.muestraFichaTecnica();
 
     }
 
